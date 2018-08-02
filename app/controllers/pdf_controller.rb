6a638d2ef4f71e534026f75487eec194f47318cc
Andrey Herrera Silva <kaherreras@unal.edu.co>
2018-08-02 21:09:36 +0000

    Tablas de direccionamiento

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -924,6 +924,64 @@ class PdfController < ApplicationController
     else
       @eco=1
     end
+    
+    @id_empresa=params[:id]
+    #@request=Request.where(:company_id => @company.id).first 
+    #@company=Company.find(@company.id) 
+    #@physical=Physical.where(:request_id => @request.id) 
+    #@logical=Logical.where(:request_id => @request.id).last 
+    #@subnets=Subnet.where(:logical_id => @logical.id) 
+    #@sizeofs=Subnet.where(:logical_id => @logical.id).size 
+    #@totalcomputers=@physical.sum(:computers) 
+    @totalcomputers2=@physical.sum(:computers)
+    @totalcomputers3=@physical.sum(:computers)
+    
+    totalcomputers=@physical.sum(:computers)
+    default=totalcomputers-@subnets.sum(:computers) 
+    @subaux=Subnet.where(:name => "default", :logical_id => @logical.id).count
+    if default>0 && @subaux==0
+      @su=Subnet.new(:name => "default", :description => "Sub red Default", :computers => default, :logical_id => @logical.id)
+      @su.save 
+    end
+    
+    #response = HTTParty.get('http://api.stackexchange.com/2.2/questions?site=stackoverflow')
+    #response = HTTParty.get("http://instanceshape.com/admin/VLSM/index.php?Host=192.168.1.24&sub[uno]=15&sub[dos]=56&sub[tres]=36")
+    require 'uri'
+    require 'net/http'
+    
+    #Direccionamiento normal
+    
+    #myurl="https://instanceshape.com/admin/VLSM/index.php?Host="+@logical.host1.to_s+"."+@logical.host2.to_s+"."+@logical.host3.to_s+"."+@logical.host4.to_s
+    myurl="https://instanceshape.com/admin/VLSM/index.php?Host="+@logical.host1.to_s+"."+@logical.host2.to_s+"."+@logical.host3.to_s+".0";
+    @subnets.each do |subs|
+      myurl+="&sub["+subs.name+"]="+subs.computers.to_s
+    end  
+    url = URI(myurl)
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(url)
+    response = http.request(request)
+    puts response.read_body
+    require 'json'
+    @h = JSON.parse response.read_body
+    
+    #Direccionamiento Escalabilidad
+    
+    #myurl="https://instanceshape.com/admin/VLSM/index.php?Host="+@logical.host1.to_s+"."+@logical.host2.to_s+"."+@logical.host3.to_s+"."+@logical.host4.to_s
+    myurl="https://instanceshape.com/admin/VLSM/index.php?Host="+@logical.host1.to_s+"."+@logical.host2.to_s+"."+@logical.host3.to_s+".0"
+    @subnets.each do |subs|
+      myurl+="&sub["+subs.name+"]="+(subs.computers+(subs.computers*@logical.scalability/100)).to_s
+    end  
+    url = URI(myurl)
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(url)
+    response = http.request(request)
+    puts response.read_body
+    require 'json'
+    @g = JSON.parse response.read_body
     render  :pdf => "Reporte", :template => 'pdf/red.html.erb'
   end
   def final
